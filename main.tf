8213af21ae66b305834bec2beccf72644b1e6d75
Conner Crosby <conner@cavcrosby.tech>
2022-09-10 22:37:44 -0400

    Integrate custom domain into the lambda
    aws_apigatewayv2_domain_name uses a ACM cert resource but it has to
    indirectly refer to the validation resource so that way Terraform does
    not try and create a custom domain name with a cert not validated yet.
    
    According to AWS's API Gateway documentation, API gateway manages the
    creation of log groups of execution logging. While I can see the
    convention used, and can probably Terraform the log group for now I am
    ok skipping on its creation (the stage name is used as part of naming
    the log group, so $default might be an issue). It seems I had an issue
    with getting the custom domain working and giving the Api Gateway
    cloudwatch permissions seemed to fix the issue.
    
    No api mapping path is needed under the custom domain name because the
    custom domain will always refer to the same api.

--- main.tf
+++ main.tf
@@ -16,13 +16,33 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to deploy project resources to."
+}
+
+# MONITOR(cavcrosby): appears there is a PR to integrate a pascal case function
+# into Terraform. If merged I should look into using it, for reference on
+# related github issue and PR:
+# https://github.com/hashicorp/terraform/issues/25230
+# https://github.com/hashicorp/terraform/pull/27357
+variable "aws_region_pascal" {
+  type        = string
+  description = "The AWS region to deploy project resources to (in Pascal case)."
+}
+
 variable "deployment_pkg_name" {
   type        = string
   description = "The deployment package file name to be consumed by the AWS Lambda service."
 }
 
+variable "lambda_fqdn" {
+  type        = string
+  description = "The fully qualified domain name in which to access the lambda by."
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_cloudwatch_log_group" "later_schedule_validator" {
@@ -62,6 +82,21 @@ resource "aws_lambda_function" "later_schedule_validator" {
   runtime          = "nodejs16.x"
 }
 
+resource "aws_acm_certificate" "later_schedule_validator" {
+  domain_name       = var.lambda_fqdn
+  validation_method = "DNS"
+
+  // Terraform docs recommend 'create_before_destroy' being set to replace a cert
+  // currently in use.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "later_schedule_validator" {
+  certificate_arn = aws_acm_certificate.later_schedule_validator.arn
+}
+
 resource "aws_sns_topic_subscription" "conner_endpoint" {
   endpoint  = "conner@cavcrosby.tech"
   protocol  = "email"
@@ -92,9 +127,35 @@ resource "aws_cloudwatch_metric_alarm" "utility_lambdas_api_count_alarm" {
   ]
 }
 
-resource "aws_cloudwatch_log_group" "utility_lambdas" {
-  name              = "/aws/apigateway/${aws_apigatewayv2_api.utility_lambdas.name}"
-  retention_in_days = 30
+# All api gateways per region share a cloudwatch role. Hence it does not make
+# sense to create a role assuming it will be isolated to this one api. For
+# reference on the scope of this role:
+# https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-logging.html#set-up-access-logging-permissions
+resource "aws_iam_role" "regional_apigateway_cloudwatch" {
+  name = "${var.aws_region_pascal}ApiGwRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = ""
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "regional_apigateway_cloudwatch" {
+  role       = aws_iam_role.regional_apigateway_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_api_gateway_account" "regional" {
+  cloudwatch_role_arn = aws_iam_role.regional_apigateway_cloudwatch.arn
 }
 
 resource "aws_apigatewayv2_route" "later_schedue_validator" {
@@ -118,6 +179,16 @@ resource "aws_lambda_permission" "utility_lambdas" {
   source_arn    = "${aws_apigatewayv2_api.utility_lambdas.execution_arn}/*/*"
 }
 
+resource "aws_apigatewayv2_domain_name" "utility_lambdas" {
+  domain_name = var.lambda_fqdn
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate_validation.later_schedule_validator.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
 resource "aws_apigatewayv2_stage" "utility_lambdas_default" {
   api_id = aws_apigatewayv2_api.utility_lambdas.id
   # Creates an identical '$default' stage for a api gateway, for reference on this
@@ -127,7 +198,14 @@ resource "aws_apigatewayv2_stage" "utility_lambdas_default" {
   auto_deploy = true
 }
 
+resource "aws_apigatewayv2_api_mapping" "utility_lambdas_default" {
+  api_id      = aws_apigatewayv2_api.utility_lambdas.id
+  domain_name = aws_apigatewayv2_domain_name.utility_lambdas.id
+  stage       = aws_apigatewayv2_stage.utility_lambdas_default.id
+}
+
 resource "aws_apigatewayv2_api" "utility_lambdas" {
-  name          = "utilityLambdasApi"
-  protocol_type = "HTTP"
+  name                         = "utilityLambdasApi"
+  protocol_type                = "HTTP"
+  disable_execute_api_endpoint = true
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,20 @@
+// Currently aws_apigatewayv2_domain_name does not export the regional endpoint
+// domain name, hence I have to use the following.
+data "aws_api_gateway_domain_name" "utility_lambdas" {
+  domain_name = var.lambda_fqdn
+}
+
 output "lambda_name" {
   description = "Name of the Lambda function."
   value       = aws_lambda_function.later_schedule_validator.function_name
 }
 
-output "default_base_url" {
-  description = "Base URL for API Gateway."
+output "custom_domain_regional_api_url" {
+  description = "URL for the custom domain regional API endpoint on the API Gateway."
+  value       = data.aws_api_gateway_domain_name.utility_lambdas.regional_domain_name
+}
+
+output "default_exec_base_url" {
+  description = "Base URL for the execute API endpoint on the API Gateway."
   value       = aws_apigatewayv2_stage.utility_lambdas_default.invoke_url
 }
